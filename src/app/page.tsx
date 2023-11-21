b956184d0cad55462242f0fc0701f061fecfe370
berkerkls <berkerkelesoglu@gmail.com>
2023-11-21 11:37:35 +0300

    profile card added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
+import { ProfileCard } from '@/components/profile-card';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Title</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      <ProfileCard />
     </main>
   );
 }
---
+++ src/components/profile-card.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import * as ProfileImage from '@/assets/berker.jpg';
+
+export const ProfileCard = () => {
+  return (
+    <div className="card w-96 bg-slate-50 shadow-xl h-32">
+      <div className="card-body">
+        <div className="flex justify-between items-center w-64">
+          <div className="avatar">
+            <div className="rounded-full">
+              <Image
+                src={ProfileImage}
+                width={50}
+                height={50}
+                alt="picture of myself"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <span className="card-title text-black">Berker Keleşoğlu</span>
+            <span className="card-text text-black text-left mt-2 text-sm">
+              Software Developer
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
