381dc35878e2abf06df6973f80ce58e955e3fbbe
berkerkls <berkerkelesoglu@gmail.com>
2023-11-28 10:14:03 +0300

    writing layout edited

--- src/app/writings/layout.tsx
+++ src/app/writings/layout.tsx
@@ -3,13 +3,5 @@ export default async function WritingLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div>
-      <div className="flex transition-colors">
-        <div className="flex justify-center w-3/4 min-h-screen">
-          <div className="p-10">{children}</div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>{children}</div>;
 }
--- src/app/writings/page.tsx
+++ src/app/writings/page.tsx
@@ -27,7 +27,7 @@ export default async function Page() {
       <MotionWrapper>
         <div className="container">
           <div className="flex flex-col justify-between">
-            <p className="text-4xl mb-2">Writings</p>
+            <p className="text-2xl mb-2">Writings</p>
             <p className="text-xl p-2 text-left font-light mb-4">
               Here is my writings that I published on Medium. Not much software
               related articles now but will be more in months for sure.🛠️
