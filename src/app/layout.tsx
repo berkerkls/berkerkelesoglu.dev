008d5892635c758cb472375347f35251622111f5
berkerkls <berkerkelesoglu@gmail.com>
2023-12-03 10:19:52 +0300

    edit:metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,11 @@ export const metadata: Metadata = {
   },
   description: 'Software Developer and Musician based in Istabnbul',
   icons: {
-    icon: '/berker.jpg',
-    apple: '/berker.jpg',
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png'],
   },
+  manifest: 'site.webmanifest',
 };
 
 export default function RootLayout({
